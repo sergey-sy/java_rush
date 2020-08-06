a8f3cfdd7fbc628be5c29dbb3b4c08ec5db110d7
sergey-sy <maiyashik@gmail.com>
2020-08-06 22:44:17 +0300

    0 уровень,  8 лекция, 2 + 3 = 5

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,17 @@
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("Это не баг, это фича.");
+        int a = 3;
+        int b = 2;
+
+        // System.out.print("два");
+        System.out.print(b);
+        System.out.print(" плюс ");
+        // System.out.print(" минус ");
+        System.out.print(a);
+        // System.out.print("три");
+        System.out.print(" равно ");
+        // System.out.print(" будет ");
+        // System.out.print("пять");
+        System.out.print(a + b);
     }
 }
