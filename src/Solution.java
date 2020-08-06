02e60e842254510d191001cf070bd8aeb38746d5
sergey-sy <maiyashik@gmail.com>
2020-08-06 22:46:44 +0300

    0 уровень,  8 лекция, Комментарии излишни

--- src/Solution.java
+++ src/Solution.java
@@ -1,17 +1,15 @@
 public class Solution {
     public static void main(String[] args) {
-        int a = 3;
-        int b = 2;
+        int x = 2;
+        int y = 12;
 
-        // System.out.print("два");
-        System.out.print(b);
-        System.out.print(" плюс ");
-        // System.out.print(" минус ");
-        System.out.print(a);
-        // System.out.print("три");
-        System.out.print(" равно ");
-        // System.out.print(" будет ");
-        // System.out.print("пять");
-        System.out.print(a + b);
+        // y = x * y;
+        y = x + y;
+
+        x = y - x;
+        y = y - x;
+
+        System.out.println(x);
+        System.out.println(y);
     }
 }
