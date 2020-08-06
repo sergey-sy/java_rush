03e791172e0dcd4c14c59760e55c899c84c61e3d
sergey-sy <maiyashik@gmail.com>
2020-08-06 22:21:23 +0300

    0 уровень,  4 лекция, Объявляем переменные

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public static void main(String[] args) {
-        String born_year = "" + (3126 - 8);
-        System.out.println(born_year);
+        String name, city;
+        int age;
     }
 }
