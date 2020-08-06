fc79def1bf09fd3bdf445112eccb2aedc46da45c
sergey-sy <maiyashik@gmail.com>
2020-08-06 22:42:07 +0300

    0 уровень,  8 лекция, Баги и фичи

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,5 @@
 public class Solution {
     public static void main(String[] args) {
-        String complient =  "Элли самая красивая";
-        System.out.println(complient);
-        System.out.println(complient);
-        System.out.println(complient);
-        System.out.println(complient);
-        System.out.println(complient);
+        System.out.println("Это не баг, это фича.");
     }
 }
